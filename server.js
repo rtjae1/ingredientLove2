b8d8bd7e7084db589c57ffaf85817127256e9b2d
rtjae1 <ryan.t.jae@gmail.com>
2019-01-01 19:19:59 -0500

    mongodb work and users route

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// db stuff
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ingredientLove")
+const db = require("./models");
+
+// 
+app.get("/allusers", function(req, res) {
+  console.log("All users route was hit!");
+  // get all users and send them back in a json blob
+  db.User
+    .find({})
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+})
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
