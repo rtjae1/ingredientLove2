31863adc2019cd80fb89f863dfeed2e27bb806dd
rtjae1 <ryan.t.jae@gmail.com>
2019-01-01 21:49:24 -0500

    simple login button

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,34 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    loggedIn: false
+  }
+
+  handleLogin = (event) => {
+    event.preventDefault();
+    console.log("login clicked");
+    console.log(this);
+    this.setState({ loggedIn: true });
+  }
 
   componentDidMount() {
     console.log("componentDidMount lifecycle method ran")
     axios.get("allusers")
-    .then(response => { console.log(response)});
+      .then(response => { console.log(response) });
   }
 
   render() {
+    let banner = this.state.loggedIn ? "Woah! You're logged in!" : "UNAUTHORIZED USER";
     return (
       <div className="App">
+        <h1>{banner}</h1>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Hello, wryan1618</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.handleLogin}>Log in to Application</button>
         </p>
       </div>
     );
