fbb7aef9a222c9df41d8a55027064765f3860ed6
rtjae1 <ryan.t.jae@gmail.com>
2019-01-11 15:22:12 -0500

    updated sign in/sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,224 @@
+// import React, { Component } from "react";
+// import axios from "axios";
+// import "./App.css";
+
+// class App extends Component {
+//   state = {
+//     loggedIn: false
+//   };
+
+//   handleLogin = event => {
+//     event.preventDefault();
+//     console.log("login clicked");
+//     console.log(this);
+//     axios
+//       .post("/login", {
+//         username: "admin",
+//         password: "test"
+//       })
+//       .then(response => {
+//         console.log(response);
+//         this.setState({ loggedIn: true, username: response.data.username });
+//       })
+//       .catch(function(error) {
+//         console.log(error);
+//       });
+//   };
+
+//   componentDidMount() {
+//     console.log("componentDidMount lifecycle method ran");
+
+//     axios.get("/user_data").then(response => {
+//       console.log(response);
+//       if (response.data.loggedIn) {
+//         this.setState({ loggedIn: true, username: response.data.username });
+//       } else {
+//         console.log("No logged in user stored in session");
+//       }
+//     });
+//   }
+
+//   render() {
+//     let banner = this.state.loggedIn
+//       ? `Woah! ${this.state.username} logged in!`
+//       : `Please log in`;
+//     return (
+//       <div className="App">
+//         <h1>{banner}</h1>
+//         <div className="App-header">
+//           <h2>Hello user</h2>
+//         </div>
+//         <p className="App-intro">
+//           {!this.state.loggedIn ? (
+//             <button onClick={this.handleLogin}>Log in to Application</button>
+//           ) : (
+//             ""
+//           )}
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { Component } from "react";
-import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
 
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
+
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
+
+  // validate form errors being empty
+  Object.values(formErrors).forEach(val => {
+    val.length > 0 && (valid = false);
+  });
+
+  // validate the form was filled out
+  Object.values(rest).forEach(val => {
+    val === null && (valid = false);
+  });
+
+  return valid;
+};
+
 class App extends Component {
-  state = {
-    loggedIn: false
-  };
+  constructor(props) {
+    super(props);
 
-  handleLogin = event => {
-    event.preventDefault();
-    console.log("login clicked");
-    console.log(this);
-    axios
-      .post("/login", {
-        username: "admin",
-        password: "test"
-      })
-      .then(response => {
-        console.log(response);
-        this.setState({ loggedIn: true, username: response.data.username });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  };
-
-  componentDidMount() {
-    console.log("componentDidMount lifecycle method ran");
-
-    axios.get("/user_data").then(response => {
-      console.log(response);
-      if (response.data.loggedIn) {
-        this.setState({ loggedIn: true, username: response.data.username });
-      } else {
-        console.log("No logged in user stored in session");
+    this.state = {
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null,
+      formErrors: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
       }
-    });
+    };
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (formValid(this.state)) {
+      console.log(`
+        --SUBMITTING--
+        First Name: ${this.state.firstName}
+        Last Name: ${this.state.lastName}
+        Email: ${this.state.email}
+        Password: ${this.state.password}
+      `);
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+    }
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let formErrors = { ...this.state.formErrors };
+
+    switch (name) {
+      case "firstName":
+        formErrors.firstName =
+          value.length < 3 ? "minimum 3 characaters required" : "";
+        break;
+      case "lastName":
+        formErrors.lastName =
+          value.length < 3 ? "minimum 3 characaters required" : "";
+        break;
+      case "email":
+        formErrors.email = emailRegex.test(value)
+          ? ""
+          : "invalid email address";
+        break;
+      case "password":
+        formErrors.password =
+          value.length < 6 ? "minimum 6 characaters required" : "";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+  };
+
   render() {
-    let banner = this.state.loggedIn
-      ? `Woah! ${this.state.username} logged in!`
-      : `Please log in`;
+    const { formErrors } = this.state;
+
     return (
-      <div className="App">
-        <h1>{banner}</h1>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello user</h2>
+      <div className="wrapper">
+        <div className="form-wrapper">
+          <h1>Create Account</h1>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                className={formErrors.firstName.length > 0 ? "error" : null}
+                placeholder="First Name"
+                type="text"
+                name="firstName"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.firstName.length > 0 && (
+                <span className="errorMessage">{formErrors.firstName}</span>
+              )}
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                className={formErrors.lastName.length > 0 ? "error" : null}
+                placeholder="Last Name"
+                type="text"
+                name="lastName"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.lastName.length > 0 && (
+                <span className="errorMessage">{formErrors.lastName}</span>
+              )}
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                className={formErrors.email.length > 0 ? "error" : null}
+                placeholder="Email"
+                type="email"
+                name="email"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.email.length > 0 && (
+                <span className="errorMessage">{formErrors.email}</span>
+              )}
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input
+                className={formErrors.password.length > 0 ? "error" : null}
+                placeholder="Password"
+                type="password"
+                name="password"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.password.length > 0 && (
+                <span className="errorMessage">{formErrors.password}</span>
+              )}
+            </div>
+            <div className="createAccount">
+              <button type="submit">Create Account</button>
+              <small>Already Have an Account?</small>
+            </div>
+          </form>
         </div>
-        <p className="App-intro">
-          {!this.state.loggedIn ? (
-            <button onClick={this.handleLogin}>Log in to Application</button>
-          ) : (
-            ""
-          )}
-        </p>
       </div>
     );
   }
